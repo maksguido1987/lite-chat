bff3f917af4b8248c7cd255a3928bdf9e3ce68b1
Your Name <you@example.com>
2021-09-26 19:37:20 +0300

    create folders server and client, setting app

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import './styles.scss';
+import IMAGE from './assets/react-logo.png';
+
+export const App = () => {
+  return (
+    <>
+      <h1>React TypeScript Webpack Started Template</h1>
+      <img src={IMAGE} alt="react-logo" width="300" height="200" />
+    </>
+  );
+};
+
+export default App;
